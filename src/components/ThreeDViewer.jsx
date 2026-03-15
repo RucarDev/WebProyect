a51e5483ac758aa90aa0808016539ee8c0ae69cb
RucarDev <carlospadillavi06@gmail.com>
2026-03-15 01:53:06 +0100

    Cambios

--- src/components/ThreeDViewer.jsx
+++ src/components/ThreeDViewer.jsx
@@ -5,16 +5,26 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const ThreeDViewer = () => {
   useEffect(() => {
+    // Crear la escena, cámara y renderizador
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.getElementById('threeDContainer').appendChild(renderer.domElement);
 
-    // Cargar el modelo GLTF
+    // Agregar luz ambiental para iluminar la escena
+    const light = new THREE.AmbientLight(0x404040, 1);  // Luz suave
+    scene.add(light);
+
+    // Agregar una luz direccional (simula luz como la del sol)
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(5, 5, 5);  // Posición de la luz
+    scene.add(directionalLight);
+
+    // Cargar el modelo 3D
     const loader = new GLTFLoader();
     loader.load(
-      '/models/grass_medium_01_1k.gltf',  // Cambia la ruta al archivo de tu modelo 3D
+      '/models/grass_medium_01_1k.gltf',  // Cambia la ruta al archivo de tu modelo GLTF
       (gltf) => {
         scene.add(gltf.scene);  // Añadir el modelo a la escena
       },
